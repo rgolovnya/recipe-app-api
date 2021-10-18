f12467e26439fd5d1b858f2e0ba8b6b9ee9f5112
rgolovnya <rgolovnya@yahoo.com>
2021-10-18 20:30:34 +0100

    added create recipe feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -79,9 +79,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
         return self.serializer_class
 
-#     def perform_create(self, serializer):
-#         """Create a new recipe"""
-#         serializer.save(user=self.request.user)
+    def perform_create(self, serializer):
+        """Create a new recipe"""
+        serializer.save(user=self.request.user)
 
 #     @action(methods=['POST'], detail=True, url_path='upload-image')
 #     def upload_image(self, request, pk=None):
