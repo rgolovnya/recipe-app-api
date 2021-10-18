f2fadc1ebbe3de02781bec376bc3eaf8081cb503
rgolovnya <rgolovnya@yahoo.com>
2021-10-18 22:45:50 +0100

    added recipe image uploaded feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,7 +1,6 @@
-# from rest_framework.decorators import action
-# from rest_framework.response import Response
-# from rest_framework import viewsets, mixins, status
-from rest_framework import viewsets, mixins
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -83,23 +82,23 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Create a new recipe"""
         serializer.save(user=self.request.user)
 
-#     @action(methods=['POST'], detail=True, url_path='upload-image')
-#     def upload_image(self, request, pk=None):
-#         """Upload an image to a recipe"""
-#         recipe = self.get_object()
-#         serializer = self.get_serializer(
-#             recipe,
-#             data=request.data
-#         )
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """Upload an image to a recipe"""
+        recipe = self.get_object()
+        serializer = self.get_serializer(
+            recipe,
+            data=request.data
+        )
 
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(
-#                 serializer.data,
-#                 status=status.HTTP_200_OK
-#             )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
 
-#         return Response(
-#             serializer.errors,
-#             status=status.HTTP_400_BAD_REQUEST
-#         )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
