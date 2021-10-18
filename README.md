ff73818a2c022a23726443c52706d4f869f1304b
rgolovnya <rgolovnya@yahoo.com>
2021-10-18 12:40:50 +0100

    added working list tags endpoint

--- README.md
+++ README.md
@@ -15,6 +15,7 @@ The API will then available at http://127.0.0.1:8000 .
 To test project, run:
 ```
 docker-compose run app sh -c "python manage.py test"
+docker-compose run --rm app sh -c "python manage.py test && flake8"
 ```
 
 other commands
@@ -23,4 +24,7 @@ docker build .
 docker-compose build
 docker-compose run app sh -c "django-admin.py startproject app ."
 docker-compose run app sh -c "python manage.py startapp core"
+docker-compose run --rm app sh -c "python manage.py startapp user"
+docker-compose run --rm app sh -c "python manage.py startapp recipe"
+docker-compose run --rm app sh -c "python manage.py makemigrations"
 ```
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,14 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+# from unittest.mock import patch
+
+from core import models
+
+
+def sample_user(email='test@test.com', password='testpass'):
+    """create a Sample User"""
+    return get_user_model().objects.create_user(email, password)
+
 
 
 class ModelTests(TestCase):
@@ -36,3 +45,41 @@ class ModelTests(TestCase):
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+    #     self.assertEqual(str(tag), tag.name)
+
+    # def test_ingredient_str(self):
+    #     """test the ingredient string representation"""
+    #     ingredient = models.Ingredient.objects.create(
+    #         user=sample_user(),
+    #         name='Cucumber'
+    #     )
+
+    #     self.assertEqual(str(ingredient), ingredient.name)
+
+    # def test_recipe_str(self):
+    #     """test the recipe string representation"""
+    #     recipe = models.Recipe.objects.create(
+    #         user=sample_user(),
+    #         title='Steak and mushroom sauce',
+    #         time_minutes=5,
+    #         price=5.00
+    #     )
+
+    #     self.assertEqual(str(recipe), recipe.title)
+
+    # @patch('uuid.uuid4')
+    # def test_recipe_file_name_uuid(self, mock_uuid):
+    #     """test that image is saved in the correct location"""
+    #     uuid = 'test-uuid'
+    #     mock_uuid.return_value = uuid
+    #     file_path = models.recipe_image_file_path(None, 'myimage.jpg')
+    #     exp_path = f'uploads/recipe/{uuid}.jpg'
+    #     self.assertEqual(file_path, exp_path)
