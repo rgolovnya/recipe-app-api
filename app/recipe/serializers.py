1e7d8779076a16ebf3d0792147b762c7d199bd1d
rgolovnya <rgolovnya@yahoo.com>
2021-10-18 19:40:08 +0100

    added list recipe feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 
-from core.models import Tag, Ingredient
-# from core.models import Tag, Ingredient, Recipe
+from core.models import Tag, Ingredient, Recipe
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -22,24 +21,24 @@ class IngredientSerializer(serializers.ModelSerializer):
         read_only_fields = ('id',)
 
 
-# class RecipeSerializer(serializers.ModelSerializer):
-#     """Serialize a recipe"""
-#     ingredients = serializers.PrimaryKeyRelatedField(
-#         many=True,
-#         queryset=Ingredient.objects.all()
-#     )
-#     tags = serializers.PrimaryKeyRelatedField(
-#         many=True,
-#         queryset=Tag.objects.all()
-#     )
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serialize a recipe"""
+    ingredients = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Ingredient.objects.all()
+    )
+    tags = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Tag.objects.all()
+    )
 
-#     class Meta:
-#         model = Recipe
-#         fields = (
-#             'id', 'title', 'ingredients', 'tags', 'time_minutes',
-#             'price', 'link'
-#         )
-#         read_only_fields = ('id',)
+    class Meta:
+        model = Recipe
+        fields = (
+            'id', 'title', 'ingredients', 'tags', 'time_minutes',
+            'price', 'link'
+        )
+        read_only_fields = ('id',)
 
 
 # class RecipeDetailSerializer(RecipeSerializer):
